730b0d82d383d91980eb64ee6bba21611c87009b
pmatsuda <pat.matsuda@comcast.net>
2019-10-14 17:10:26 -0500

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,64 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch');
 var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify-es').default;
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
-var merge = require('merge-stream');
 var scss = require('gulp-sass');
 
-function version(){
-  var now = new Date(),
-    Y = now.getFullYear(),
-    m = now.getMonth()+1,
-    d = now.getDate(),
-    H = now.getHours(),
-    i = now.getMinutes(),
-    s = now.getSeconds();
+/**
+ * Define the paths upon which the files to be *gulped* will resided.
+ * # styles - the name of a collection (this can be anything you like)
+ * # src - the files to be gulped, multiple paths would be listed inside of an 
+ * array
+ * # dest - the output location
+ * 
+ */
+var paths = {
+  styles: {
+    src: 'src/scss/**.scss',
+    dest: 'dist/css/',
+  }
+};
 
-    if(H < 10) {
-        H = '0' + H;
-    }
-
-    if(i < 10) {
-        i = '0' + i;
-    }
-
-    if(s < 10) {
-        s = '0' + s;
-    }
-
-    return String(10000*Y + 100*m + d + '.' + H + i + s);
+/**
+ * Define our tasks using plain functions
+ */
+function buildCSS() {
+  return gulp.src(paths.styles.src)
+    .pipe(scss())
+    .pipe(cleanCSS())
+    // pass in options to the stream
+    .pipe(rename({
+      basename: 'main',
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(paths.styles.dest));
 }
 
-gulp.task('default', ['watch']);
+/**
+ * Define a list of tasks to be executed when `gulp watch` is executed.
+ */
+function watch() {
+  gulp.watch(paths.styles.src, buildCSS);
+}
 
-gulp.task('build-css', function(){
-  //Create an unminified version
-  var full = gulp.src([
-    'src/scss/main.scss'
-  ])
-  . pipe(scss())
-  . pipe(concat('main.css'))
-  . pipe(gulp.dest('dist/css'));
+exports.buildCSS = buildCSS;
+exports.watch = watch;
 
-  //Create a minified version
-  var min = gulp.src([
-    'src/scss/main.scss'
-  ])
-  . pipe(scss())
-  . pipe(cleanCSS())
-  . pipe(concat('main.min.css'))
-  . pipe(gulp.dest('dist/css'));
+/**
+ * Specify if tasks run in series or parallel using `gulp.series` and 
+ * `gulp.parallel`
+ */
+var build = gulp.series(gulp.parallel(buildCSS));
 
-  return merge(full, min);
-});
+/**
+ * Create a list of tasks that can be ran manually
+ * Running `gulp build` fom the cli would execute the build task
+ */
+gulp.task('build', build);
 
-
-gulp.task('build-js', function() {
-  var full = gulp.src([
-    'src/js/main.js'
-  ])
-  .pipe(concat('main.js'))
-  .pipe(gulp.dest('dist/js'));
-
-  var min = gulp.src([
-    'src/js/main.js'
-  ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('dist/js'));
-
-  return merge(full, min);
-});
-
-gulp.task('watch', function(){
-  gulp.watch('./src/scss/**/*.scss', ['build-css']);
-  gulp.watch('./src/js/**/*.js', ['build-js']);
-});
+/**
+ * Define default task that can be called by just running `gulp` from cli
+ * this would be the same as running `gulp watch`
+ */
+gulp.task('default', watch);
